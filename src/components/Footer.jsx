9210ee0b7801b1155ecec28e0d2b230a11993656
Thisalzzz <wasanthaakurgoda@gmail.com>
2025-10-02 19:43:54 +0530

    number

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
                 <svg className="h-5 w-5 mr-3 text-[#e6d5c3]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                +94 112 345 678
+                +94 70 784 5444
               </p>
               <p className="flex items-center">
                 <svg className="h-5 w-5 mr-3 text-[#e6d5c3]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
